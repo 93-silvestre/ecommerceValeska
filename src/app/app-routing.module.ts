4469e7049365293c883c3bc35098b26974ce580d
93-silvestre <nathyribsilvestre@gmail.com>
2021-11-25 18:04:07 -0300

    Implementação de rotas do painel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
+import { PainelComponent } from './painel/painel.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: 'cadastro', pathMatch: 'full'},
   {path: 'cadastro', component: CadastroComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'painel', component: PainelComponent}
 ];
 
 @NgModule({
